6d1863cdf1c1d3cb73122bcf77e3d7ca818f13d0
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-10-09 18:16:38 +0300

    fix lab2

--- semester1/labs/lab2/main1.c
+++ semester1/labs/lab2/main1.c
@@ -2,34 +2,26 @@
 #include<math.h>
 
 double f(unsigned n, unsigned* cntOperations) {
-
   double result = 1;
   unsigned cnt = 4;     // =1 | =1 | *cntOperations | = 
-
   for (int i = 1; i <= n; i++) {
-
     double denominator = 0.0;
     for (int j = 1; j <= i; j++) {
       denominator = denominator + sin(j);
-      cnt += 6;                           // <= | ++ | = | + | sin | jmp
+      cnt += 6;         // <= | ++ | = | + | sin | jmp
     }
-    
-    result = result * ( ( cos(i) + 1 ) / denominator );
-    cnt += 10;            // <= | ++ | =0.0 | =1 | = | * | cos | + | / | jmp 
+    result = result * ((cos(i) + 1) / denominator);
+    cnt += 10;          // <= | ++ | =0.0 | =1 | = | * | cos | + | / | jmp 
   }
-  
   *cntOperations = cnt;
   return result;
 }
 
 int main(int argc, char* argv[]) {
   unsigned n, countOfOperations;
-
   printf("Enter natural number: ");
   scanf("%u", &n);
-
   double res = f(n, &countOfOperations);
   printf("f(%d) = %.7lf\nCount of operations = %u\n", n, res, countOfOperations);
-
   return 0;
 }
--- semester1/labs/lab2/main2.c
+++ semester1/labs/lab2/main2.c
@@ -2,34 +2,23 @@
 #include<math.h>
 
 double f(unsigned n, unsigned* cntOperations) {
-
   double result = 1;
   unsigned cnt = 5;    // =1 | =0.0 | =1 | *cntOperations | = 
-
   double sinsSum = 0.0;
-
   for (int i = 1; i <= n; i++) {
-
     sinsSum = sinsSum + sin(i);
-    
-    result = result * ( ( cos(i) + 1 ) / sinsSum );
-
-    cnt += 11;            // <= | ++ | = | + | sin | = | * | cos | + | / | jmp
+    result = result * ((cos(i) + 1) / sinsSum);
+    cnt += 11;         // <= | ++ | = | + | sin | = | * | cos | + | / | jmp
   }
-  
   *cntOperations = cnt;
   return result;
 }
 
 int main(int argc, char* argv[]) {
   unsigned n, countOfOperations;
-
   printf("Enter natural number: ");
   scanf("%u", &n);
-
   double res = f(n, &countOfOperations);
-
   printf("f(%d) = %.7lf\nCount of operations = %u\n", n, res, countOfOperations);
-
   return 0;
 }
