69ff503015516e66d792e453d951508d514d66e1
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-10-09 18:24:52 +0300

    fix first lab

--- semester1/labs/lab1/main2.c
+++ semester1/labs/lab1/main2.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
 
 int main(int argc, char* argv[]) {
-    float x, y;
-    
-    printf("Enter value for x: ");
-    scanf("%f", &x);
-
-    if (x > -15 && x <= 3) {
-        y = 4 * x*x + 2;
-        printf("f(%.2f) = %.2f\n", x, y);
+  float x, y;
+  printf("Enter value for x: ");
+  scanf("%f", &x);
+  if (x > -15 && x <= 3) {
+    y = 4 * x*x + 2;
+    printf("f(%.2f) = %.2f\n", x, y);
+  }
+  else {
+    if (x <= -30 || x > 20) {
+      y = (3 * x*x*x) / 4 - 5;
+      printf("f(%.2f) = %.2f\n", x, y);
     }
     else {
-        if (x <= -30 || x > 20) {
-            y = (3 * x*x*x) / 4 - 5;
-            printf("f(%.2f) = %.2f\n", x, y);
-        }
-        else {
-            printf("no value\n");
-        }
+      printf("no value\n");
     }
-
-    return 0;
+  }
+  return 0;
 }
