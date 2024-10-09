51aa955a2c194db7b16672ed8c7639dba4eec5a2
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-10-09 18:30:55 +0300

    fix lab4

--- semester1/labs/lab4/main.c
+++ semester1/labs/lab4/main.c
@@ -2,43 +2,35 @@
 
 void spiral(const int rows, const int cols, const int printDelay, const char symbol) {
   if (rows > cols) return;
-
   const int step = cols - rows + 2;
   int row = rows / 2;
   int col = (cols - step) / 2 + step - 1;
-
   for (int i = 0; i < rows; i++) {
-    
     if (i % 2 == 0) {
       for (int j = 0; j < i + step; j++) {
         mvaddch(row, col - j, symbol);
         refresh();
         napms(printDelay);
       }
-
       for (int j = 0; j < i; j++) {
         mvaddch(row - j - 1, col - i - step + 1, symbol);
         refresh();
         napms(printDelay);
       }
-
       row -= i + 1;
       col -= i + step - 1;
     }
-
     else {
       for (int j = 0; j < i + step; j++) {
         mvaddch(row, col + j, symbol);
         refresh();
         napms(printDelay);
       }
-
       for (int j = 0; j < i; j++) {
         mvaddch(row + j + 1, col + i + step - 1, symbol);
         refresh();
         napms(printDelay);
       }
-    
       row += i + 1;
       col += i + step - 1;
     }
